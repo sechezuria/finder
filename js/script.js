95ff31649cca2d043ccdef8370701aeae5ab5c20
Saul Echezuria <saulechezuriarojas@gmail.com>
2023-03-27 18:01:41 -0300

    "actualizacion"

--- js/script.js
+++ js/script.js
@@ -1,19 +1,4 @@
-// Función que cambia el color de fondo del encabezado al hacer clic en el botón
-function changeColor() {
-    var header = document.querySelector('header');
-    header.style.backgroundColor = 'pink';
-  }
-  
-  // Galería de imágenes que se desplazan automáticamente
-  var slideIndex = 0;
-  
-  function showSlides() {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1}
-    slides[slideIndex-1].style.display = "block";
-    setTimeout(showSlides, 2000); }// Cambia la imagen cada 2 segundos}
+_toggle.onclick = () =>{
+    _items.classList.toggle("open")
+    _toggle.classList.toggle("close")
+}
